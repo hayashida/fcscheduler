64baecae14b9a510803046d7458b7da65134ac21
林田康裕 <yan@yan.local>
2019-10-06 16:54:53 +0900

    add menu

---
+++ app/controllers/PlacesController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javax.inject.Inject;
+import play.mvc.*;
+
+public class PlacesController extends Controller {
+
+  @Inject
+  public PlacesController() {}
+
+  public Result index() {
+    return TODO();
+  }
+
+  public Result add() {
+    return TODO();
+  }
+}
